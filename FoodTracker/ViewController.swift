b441389951ae8cb52fb754936104d1407be4a350
Renz <renz@Renzs-iMac.local>
2018-02-05 22:02:18 -0800

    start lab 4

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: Properties
+    
+    // MARK: Delegate Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +22,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    // MARK: Actions
 
 }
 
