7ff420767399a1d3e9af592d1b0dca97cd341f5b
Kerensa E Mattison <mattison@icsmac06.local>
2018-02-08 09:41:38 -0800

    End of creating gesture recognizer

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import os
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     // MARK: Properties
     @IBOutlet weak var photoImageView: UIImageView!
     // MARK: Delegate Methods
@@ -17,7 +18,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        self.present(imagePickerController, animated:true, completion:nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController,
+    didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as?
+            UIImage else {
+                os_log("Missing image in %@", log: OSLog.default, type: .debug, info)
+                return
+        }
+        photoImageView.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
