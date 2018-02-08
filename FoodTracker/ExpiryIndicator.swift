7a155181bfbf965c3710c3d22c14208c1941b4be
Kerensa E Mattison <mattison@icsmac06.local>
2018-02-08 10:58:20 -0800

    middle of adding expiry indicator

---
+++ FoodTracker/ExpiryIndicator.swift
@@ -0,0 +1,33 @@
+//
+//  ExpiryIndicator.swift
+//  FoodTracker
+//
+//  Created by  Kerensa E Mattison on 2018-02-08.
+//  Copyright © 2018  Kerensa E Mattison. All rights reserved.
+//
+
+import UIKit
+
+class ExpiryIndicator: UIStackView {
+    private var indicators = [UIImageView]()
+    private let indicatorCount: Int = 10
+    var indicatorPercentage: Int = 100 {
+        didSet {
+            for i in 0..<indicatorCount {
+                indicators[i].alpha = i * indicatorCount < indicatorPercentage ? 1.0 : 0.1
+            }
+        }
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        for _ in 0..<self.indicatorCount {
+            let indicator = UIImageView(image: #imageLiteral(resourceName:"defaultImage"))
+            indicator.contentMode = .scaleToFill
+            self.addArrangedSubview(indicator)
+            indicators.append(indicator)
+        }
+    }
+    
+
+}
