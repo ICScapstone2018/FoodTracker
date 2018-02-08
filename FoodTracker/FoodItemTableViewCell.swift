6a007a4fa38602b09f90b8c8538217809a0fe4a1
Kerensa E Mattison <mattison@icsmac06.local>
2018-02-08 12:43:42 -0800

    have to add HorizontalStackView

---
+++ FoodTracker/FoodItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FoodItemTableViewCell.swift
+//  FoodTracker
+//
+//  Created by  Kerensa E Mattison on 2018-02-08.
+//  Copyright © 2018  Kerensa E Mattison. All rights reserved.
+//
+
+import UIKit
+
+class FoodItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
